04e4468621bb651379baa4db589e8e981ce76b30
Alena Zhmurko <alenazhmurko@gmail.com>
2020-07-15 00:38:49 +0300

    Updates

---
+++ precache-manifest.6b1a66d348c7d0e1f57438c233172af5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8111c4a5997042d1a0f4c409ce77f193",
+    "url": "/Front-End-Web-UI-Frameworks-and-Tools-Planner/index.html"
+  },
+  {
+    "revision": "001cb07330ad5926dfeb",
+    "url": "/Front-End-Web-UI-Frameworks-and-Tools-Planner/static/css/main.ab7da631.chunk.css"
+  },
+  {
+    "revision": "607619fdab7801c434a5",
+    "url": "/Front-End-Web-UI-Frameworks-and-Tools-Planner/static/js/2.a81b8e12.chunk.js"
+  },
+  {
+    "revision": "001cb07330ad5926dfeb",
+    "url": "/Front-End-Web-UI-Frameworks-and-Tools-Planner/static/js/main.9508badd.chunk.js"
+  },
+  {
+    "revision": "a3d8707662de39665fa1",
+    "url": "/Front-End-Web-UI-Frameworks-and-Tools-Planner/static/js/runtime~main.598f29a0.js"
+  }
+]);
